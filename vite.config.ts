a76aab9d05134cb58edffcfae0c9e6b0c228c90a
Hamid Kano <h.kano@almusand.co>
2025-09-05 06:59:50 +0300

    لإ

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: true,
     rollupOptions: {
       output: {
         manualChunks: undefined
